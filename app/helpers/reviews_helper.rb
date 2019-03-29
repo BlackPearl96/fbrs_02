22bc635269fce168270d48d225b5fac1424575d4
BlackPearl96 <van9665@gmail.com>
2019-03-29 11:03:19 +0700

    gem_devise_users

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,5 +1,5 @@
 module ReviewsHelper
-  def load_by_reviews
-    rv = current_user.reviews.find {|review| review.book_id == @book.id}
-  end
+  # def load_by_reviews
+  #   rv = current_user.reviews.find {|review| review.book_id == @book.id}
+  # end
 end
